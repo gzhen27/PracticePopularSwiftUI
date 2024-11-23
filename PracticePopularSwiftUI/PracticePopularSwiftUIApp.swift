5c43171df3ad1ee3eda2d2a12547864242b1d211
Gray.Z <grayzprogrammer@gmail.com>
2024-11-23 13:30:59 -0600

    Initial Commit

---
+++ PracticePopularSwiftUI/PracticePopularSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticePopularSwiftUIApp.swift
+//  PracticePopularSwiftUI
+//
+//  Created by G Zhen on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct PracticePopularSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
